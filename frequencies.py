b7b3c2c2f1a62908dada00fd5779d3479a908b40
Sujan Naik <sujannaik047@gmail.com>
2023-12-04 12:26:42 +0000

    Finish exercise

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,18 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
+def toString(item):
+    if type(item) is not str:
+        return str(item)
+    return item
+
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    string_items = map(toString, items)
+    for string_item in string_items:
+        if frequencies.keys().__contains__(string_item):
+            frequencies[string_item] += 1
+        else:
+            frequencies[string_item] = 1
     return frequencies
